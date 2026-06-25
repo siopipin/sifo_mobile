b1917140a964f8b7c1faaf61597ded8ac9d28bd1
siopipin <siojurnalispipin@hotmail.com>
2026-06-25 09:02:53 +0700

    update for last release

---
+++ android/app/src/main/kotlin/com/mystikesgs/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.mystikesgs.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/stikesgs/app/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.stikesgs.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-
-class MainActivity: FlutterActivity() {
-}
